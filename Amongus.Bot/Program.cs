bdfb350b187ee89a3c4781e7f62cf4fabbbf5b81
Thorstein Løkensgard <loekensgard@gmail.com>
2020-09-19 19:16:35 +0200

    removed unused code

--- Amongus.Bot/Program.cs
+++ Amongus.Bot/Program.cs
@@ -24,53 +24,5 @@ namespace Amongus.Bot
                 Console.WriteLine(e.StackTrace);
             }
         }
-
-        public Program()
-        {
-            // It is recommended to Dispose of a client when you are finished
-            // using it, at the end of your app's lifetime.
-            _client = new DiscordSocketClient();
-
-            _client.Log += LogAsync;
-            _client.Ready += ReadyAsync;
-            _client.MessageReceived += MessageReceivedAsync;
-        }
-
-        public async Task MainAsync()
-        {
-            // Tokens should be considered secret data, and never hard-coded.
-            await _client.LoginAsync(TokenType.Bot, "NzU2ODk2NDAzMzY3OTE5NjQ4.X2YgtQ.tUb-kkciSUSU4LQrtqRwNsz7lY0");
-            await _client.StartAsync();
-
-            // Block the program until it is closed.
-            await Task.Delay(Timeout.Infinite);
-        }
-
-        private Task LogAsync(LogMessage log)
-        {
-            Console.WriteLine(log.ToString());
-            return Task.CompletedTask;
-        }
-
-        // The Ready event indicates that the client has opened a
-        // connection and it is now safe to access the cache.
-        private Task ReadyAsync()
-        {
-            Console.WriteLine($"{_client.CurrentUser} is connected!");
-
-            return Task.CompletedTask;
-        }
-
-        // This is not the recommended way to write a bot - consider
-        // reading over the Commands Framework sample.
-        private async Task MessageReceivedAsync(SocketMessage message)
-        {
-            // The bot should never respond to itself.
-            if (message.Author.Id == _client.CurrentUser.Id)
-                return;
-
-            if (message.Content == "!flip")
-                await message.Channel.SendMessageAsync("heads!");
-        }
     }
 }
